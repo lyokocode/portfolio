70980b8db5e4938e3f7fb911693dbe80f0613712
lyokocode <aelita.dev@gmail.com>
2023-09-25 23:00:26 +0300

    footer UI 1.0

--- client/src/components/footer/index.jsx
+++ client/src/components/footer/index.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
-
+import { Link } from "react-router-dom"
+import { AiFillLinkedin, AiOutlineGithub, AiOutlineTwitter } from "react-icons/ai"
+import "./footer.scss"
 export const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer className="footer">
+            <div className="info">
+                <div className="logo">
+                    <img src="/logo.png" alt="aelita blog" width={50} height={50} className="img" />
+                    <h1 className="logoText">Aelita Blog</h1>
+                </div>
+                <p className="desc">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim
+                    necessitatibus similique aspernatur obcaecati veritatis. Aperiam cum
+                    porro sequi, totam minima consequuntur, aspernatur deleniti vero
+                    repellendus dorales.
+                </p>
+                <div className="icons">
+                    <a href="#">
+                        <AiFillLinkedin size={24} />
+                    </a>
+                    <a href="#">
+                        <AiOutlineGithub size={24} />
+                    </a>
+                    <a href="#">
+                        <AiOutlineTwitter size={24} />
+                    </a>
+                </div>
+            </div>
+            <div className="links">
+                <div className="list">
+                    <span className="listTitle">Links</span>
+                    <Link to="/">Homepage</Link>
+                    <Link to="/blogs">Blogs</Link>
+                    <Link to="/">projects</Link>
+                    <Link to="/">Contact</Link>
+                </div>
+                <div className="list">
+                    <span className="listTitle">Tags</span>
+                    <Link to="/">React</Link>
+                    <Link to="/">NextJS</Link>
+                    <Link to="/">JavaScript</Link>
+                    <Link to="/">Scss</Link>
+                </div>
+                <div className="list">
+                    <span className="listTitle">Social</span>
+                    <Link to="/">github</Link>
+                    <Link to="/">linkedin</Link>
+                    <Link to="/">twitter</Link>
+                </div>
+            </div>
+        </footer>
+
     )
 }
