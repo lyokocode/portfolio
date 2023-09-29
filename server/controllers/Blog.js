1a3237a3ff93ceb1171f0c4d132946375cc00a16
lyokocode <aelita.dev@gmail.com>
2023-09-29 17:15:56 +0300

    error handling

--- server/controllers/Blog.js
+++ server/controllers/Blog.js
@@ -1,12 +1,14 @@
 import { Blog } from "../models/Blog.js";
+import { createError } from "../utils/error.js";
 
 // GET ALL BLOGS
-export const getAllBlogs = async (req, res) => {
+export const getAllBlogs = async (req, res, next) => {
+
     try {
         const blogs = await Blog.findAll()
         return res.status(200).json(blogs)
     } catch (err) {
-        console.log(err)
+        next(err)
     }
 }
 
@@ -29,12 +31,11 @@ export const getBlog = async (req, res) => {
         const blog = await Blog.findByPk(id);
 
         if (!blog) {
-            return res.status(404).json({ error: 'Blog bulunamadı' });
+            return res.status(404).json({ error: 'Blog is not defined' });
         }
 
         res.json(blog);
     } catch (error) {
-        console.error('Blog sorgulama hatası:', error);
-        res.status(500).json({ error: 'Blog sorgulama hatası' });
+        console.error(error);
     }
 }
--- server/index.js
+++ server/index.js
@@ -13,9 +13,23 @@ app.use(express.json());
 // Routes
 app.use("/api/blogs", blogRoute)
 
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500
+    const errorMessage = err.message || "Something went wrong"
+
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorStatus,
+        stack: err.stack
+    })
+})
+
 async function main() {
     try {
-        await sequelize.sync({ force: true });
+        await sequelize.sync(
+            // { force: true }
+        );
         console.log("db connection is successfull")
         app.listen(process.env.PORT, () => console.log(`api is running on port: ${process.env.PORT}`))
 
