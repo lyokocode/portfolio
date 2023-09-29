368b3fa78d9bf76d4c0640f65ec8a4d2de9aa023
lyokocode <aelita.dev@gmail.com>
2023-09-29 15:24:26 +0300

    server is running

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from "express"
+import dotenv from "dotenv"
+
+
+const app = express()
+
+// middlewares
+dotenv.config()
+
+async function main() {
+    try {
+        app.listen(process.env.PORT, () => console.log(`api is running on port: ${process.env.PORT}`))
+
+    } catch (error) {
+        console.log(`Unable to connect to the database ${error}`)
+    }
+}
+main()
