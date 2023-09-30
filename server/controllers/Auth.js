a84b656b62a6d9313083106be02eab2d8a520e38
lyokocode <aelita.dev@gmail.com>
2023-09-30 22:36:49 +0300

    hash password

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -1,15 +1,19 @@
 import { User } from "../models/User.js";
+import bcrypt from "bcryptjs"
 import { createError } from "../utils/error.js";
 
 export const register = async (req, res, next) => {
-    console.log("first")
+
+    const salt = bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(req.body.password, salt)
+
     try {
         const newUser = await User.create({
             fullName: req.body.fullName,
             userName: req.body.userName,
             email: req.body.email,
             avatar: req.body.avatar,
-            password: req.body.password
+            password: hash
         });
         return res.status(201).json(newUser);
 
