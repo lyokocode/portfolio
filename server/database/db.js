6f94f987e65bf71e92245da9f7c08c1628e61fdc
lyokocode <aelita.dev@gmail.com>
2023-10-14 14:58:47 +0300

    database model changed

--- server/database/db.js
+++ server/database/db.js
@@ -1,6 +1,8 @@
 import Sequelize from "sequelize"
+import pg from 'pg';
 
 export const sequelize = new Sequelize('postgres', 'postgres', 'IteWhAbEgEI9zEyc', {
     host: 'db.bizdptqtvsjekgsblenm.supabase.co',
-    dialect: 'postgres'
+    dialect: 'postgres',
+    dialectModule: pg
 })
