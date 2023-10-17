e19e1f9b4e8961e9b982adf1aecbab39a90b2ccc
lyokocode <aelita.dev@gmail.com>
2023-10-17 22:31:38 +0300

    navbar UI 1.0

--- admin/src/components/Navbar/index.jsx
+++ admin/src/components/Navbar/index.jsx
@@ -1,10 +1,30 @@
 import "./navbar.scss"
+{/* <img src={`${import.meta.env.VITE_REACT_SUPABASE_STORAGE}/object/public/blog/images/${blog.image}?t=2023-10-13T10%3A09%3A55.558Z`} alt="" className="image" /> */ }
+import { useSelector } from 'react-redux'
 
 
 export const Navbar = () => {
+    const { user } = useSelector(state => state.auth)
+    console.log(user)
+
     return (
-        <div className="navbar">
-            Navbar
-        </div>
+        <nav className="navbar">
+            <div className="logo">
+                {"<Aelita />"}
+            </div>
+            <div className="userInfo">
+                <>
+                    <div className="userImage">
+                        <img src={`${import.meta.env.VITE_REACT_SUPABASE_STORAGE}/object/public/blog/user/${user?.avatar}`} alt="" className="image" />
+
+                    </div>
+                    <p className="userName"> {user?.fullName}</p>
+                </>
+            </div>
+            <button className="logoutBtn">
+                log out
+            </button>
+
+        </nav>
     )
 }
