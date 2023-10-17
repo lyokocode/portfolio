28eb97c7ba6bd8649ba345248b7e8274026ab295
lyokocode <aelita.dev@gmail.com>
2023-10-17 21:51:14 +0300

    private route user control globaly

--- admin/src/utils/PrivateRoute.jsx
+++ admin/src/utils/PrivateRoute.jsx
@@ -1,8 +1,8 @@
 import { Outlet, Navigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 export const PrivateRoutes = () => {
-    const user = false
-
+    const { user } = useSelector(state => state.auth)
     return (
         user ? <Outlet /> : <Navigate to="/login" />
     )
