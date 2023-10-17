675b99d2c06cf24b5c89a88e6bb242182119fe08
lyokocode <aelita.dev@gmail.com>
2023-10-17 21:03:52 +0300

    login page UI

--- admin/src/pages/Login/index.jsx
+++ admin/src/pages/Login/index.jsx
@@ -1,7 +1,53 @@
+import { useState } from "react"
 import "./login.scss"
-
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai"
 export const Login = () => {
+
+    const [view, setView] = useState(false)
     return (
-        <div>Login</div>
+        <>
+
+            <div className="formWrapper">
+
+                <div className="loginForm">
+                    <div className="logo">
+                        {"<Aelita />"}
+                    </div>
+                    <div className="formItem">
+                        <input
+                            type="email"
+                            className="inputForm"
+                            placeholder="e-mail"
+                        />
+                    </div>
+                    <div className="formItem">
+                        <input
+                            type={view ? "text" : "password"}
+                            className="inputForm"
+                            placeholder="password"
+                        />
+                        <div
+                            onClick={() => setView(!view)}
+                            className="passwordView"
+                        >
+                            {view ? <AiFillEyeInvisible /> : <AiFillEye />}
+                        </div>
+                    </div>
+                    <div className="formItem">
+                        <button
+                            type="submit"
+                            className="loginBtn pull-right"
+                            value="Log In"
+                        >
+                            Log In
+                        </button>
+                        <input
+                        />
+                        <div className="clear-fix"></div>
+                    </div>
+                </div>
+            </div>
+
+        </>
     )
 }
