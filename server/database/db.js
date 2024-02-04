91b13a6c0c0e98f738e9cfc929bc89ad8159f7d4
lyokocode <aelita.dev@gmail.com>
2024-02-05 01:58:43 +0300

    new supabase uri

--- server/database/db.js
+++ server/database/db.js
@@ -4,8 +4,10 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-export const sequelize = new Sequelize(process.env.SUPABASE_DB_OWNER, process.env.SUPABASE_DB_NAME, process.env.SUPABASE_PASSWORD, {
-    host: process.env.SUPABASE_HOST,
-    dialect: 'postgres',
-    dialectModule: pg
-})
+// export const sequelize = new Sequelize(process.env.SUPABASE_DB_OWNER, process.env.SUPABASE_DB_NAME, process.env.SUPABASE_PASSWORD, {
+//     host: process.env.SUPABASE_HOST,
+//     dialect: 'postgres',
+//     dialectModule: pg,
+// })
+
+export const sequelize = new Sequelize(process.env.SUPABASE_URI);
